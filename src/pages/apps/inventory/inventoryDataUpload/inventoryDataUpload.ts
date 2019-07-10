6901992fa229415c81bee647742be19e32cb708e
731646594 <731646594@qq.com>
2019-07-10 14:46:51 +0800

    0.4.3 5、修改部分盘点数据上传 4、优化inventory 3、优化approval 2、优化query和queryDetail 1、优化application

--- src/pages/apps/inventory/inventoryDataUpload/inventoryDataUpload.ts
+++ src/pages/apps/inventory/inventoryDataUpload/inventoryDataUpload.ts
@@ -12,6 +12,8 @@ export class InventoryDataUploadPage {
   userCode;
   departName;
   departCode;
+  newPlanDetail=[];
+  existPlanDetail=[];
   constructor(public navCtrl: NavController,public httpService:HttpService,public storageService:StorageService,
               public alertCtrl:AlertController,public loadingCtrl:LoadingController,public navParams:NavParams,public app:App) {
     this.loadData();
@@ -23,23 +25,22 @@ export class InventoryDataUploadPage {
     this.userCode = this.storageService.read("loginUserCode");
     this.departName = this.storageService.read("loginDepartName");
     this.departCode = this.storageService.read("loginDepartCode");
-    let newPlanDetail = [];
-    let existPlanDetail = [];
+    this.planDetailList = [];
     this.storageService.getUserTable().executeSql(this.storageService.getSSS("newPlanDetail",this.userCode),[]).then(res=>{
       if (res.rows.length>0){
-        newPlanDetail = JSON.parse(res.rows.item(0).stringData);
-        for (let i in newPlanDetail){
-          newPlanDetail[i]["planStatus"] = "new";
-          this.planDetailList.push(newPlanDetail[i]);
+        this.newPlanDetail = JSON.parse(res.rows.item(0).stringData);
+        for (let i in this.newPlanDetail){
+          this.newPlanDetail[i]["planStatus"] = "new";
+          this.planDetailList.push(this.newPlanDetail[i]);
         }
       }
     }).catch(e =>alert("erro2_1:"+JSON.stringify(e)));
     this.storageService.getUserTable().executeSql(this.storageService.getSSS("existPlanDetail",this.userCode),[]).then(res=>{
       if (res.rows.length>0){
-        existPlanDetail = JSON.parse(res.rows.item(0).stringData);
-        for (let i in existPlanDetail){
-          existPlanDetail[i]["planStatus"] = "exist";
-          this.planDetailList.push(existPlanDetail[i]);
+        this.existPlanDetail = JSON.parse(res.rows.item(0).stringData);
+        for (let i in this.existPlanDetail){
+          this.existPlanDetail[i]["planStatus"] = "exist";
+          this.planDetailList.push(this.existPlanDetail[i]);
         }
       }
     }).catch(e =>alert("erro2_2:"+JSON.stringify(e)));
@@ -47,7 +48,7 @@ export class InventoryDataUploadPage {
   uploadList(){
     let loadingCtrl = this.loadingCtrl.create({
       content:"正在加载",
-      duration:10000
+      duration:5000
     });
     loadingCtrl.present();
     this.httpService.post(this.httpService.getUrl()+"cellPhoneController.do?uploadcheckplan",{userCode:this.userCode,departCode:this.departCode,uploadType:"",uploadFile:[],keyCode:"",data:""}).subscribe(data=>{
@@ -57,9 +58,38 @@ export class InventoryDataUploadPage {
           title:data.msg
         });
         alert.present();
+        this.storageService.deleteUserTable("newPlanDetail",this.userCode);
+        this.storageService.deleteUserTable("existPlanDetail",this.userCode);
       }else {
         alert(data.msg)
       }
     })
+  }
+  testDelete(index){
+    let alertCtrl = this.alertCtrl.create({
+      title:"是否删除该条数据？",
+      buttons:[
+        {
+          text:"是",
+          handler:()=>{
+            let l = index-this.newPlanDetail.length;
+            if(l>0){
+              this.existPlanDetail.splice(l,1);
+              this.storageService.updateUserTable("existPlanDetail",this.userCode,JSON.stringify(this.existPlanDetail));
+            }
+            else {
+              this.newPlanDetail.splice(index,1);
+              this.storageService.updateUserTable("newPlanDetail",this.userCode,JSON.stringify(this.newPlanDetail));
+            }
+            this.loadData();
+          }
+        },
+        {
+          text:"否"
+        }
+      ]
+    });
+    alertCtrl.present();
+
   }
 }
