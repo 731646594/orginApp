ad567bf77da6b494c6933dbd8b2dc01e219b0484
731646594 <731646594@qq.com>
2019-09-05 13:34:04 +0800

    0.5.0 1、修改启动页图 2、修改用户头像 3、修复盘点数据上传图片数据的bug 4、修复资产查询和台账查询的图片显示的bug 5、盘盈原因和存放地点可输入

--- src/pages/commonStyle/selectFilter/selectFilter.ts
+++ src/pages/commonStyle/selectFilter/selectFilter.ts
@@ -13,11 +13,16 @@ export class SelectFilterPage {
   showData=[];
   page=0;
   isScrollWork = false;
+  inputValue;
+  placeholderString="请输入要筛选的字符"
   constructor(public navCtrl: NavController,public navParams:NavParams,public viewCtrl:ViewController,
               public toastCtrl:ToastController,public events:Events) {
     this.data = this.navParams.get('data');
     this.optionValue = this.navParams.get('optionValue');
     this.optionName = this.navParams.get('optionName');
+    if(this.optionName == "complexname"){
+      this.placeholderString="请输入要筛选或输入的字符"
+    }
     this.filterData = JSON.parse(JSON.stringify(this.data));
     this.initializeItems()
   }
@@ -46,6 +51,9 @@ export class SelectFilterPage {
   }
   getItems(ev) {
     var val = ev.target.value;
+    if(this.optionName == "complexname"){
+      this.inputValue = val
+    }
     if (val && val.trim() != '') {
       this.filterData = this.data.filter((item) => {
         return (item[this.optionName].indexOf(val) > -1);
@@ -58,7 +66,12 @@ export class SelectFilterPage {
     this.initializeItems();
   }
   back(){
-    let modelData: string = '-1';
+    let modelData;
+    if(this.optionName == "complexname"){
+      modelData = {selectedValue:this.inputValue,selectedName:this.inputValue}
+    }else {
+      modelData = '-1';
+    }
     this.viewCtrl.dismiss(modelData);
   }
 
