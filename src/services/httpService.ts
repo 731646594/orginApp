a0f854e8e0e876a827de3f032008ce400dcfa24d
731646594 <731646594@qq.com>
2020-03-26 16:57:53 +0800

    0.81 1、修复盘点数据上传图片相关问题 2、新增RFID特殊盘点 3、启用在线升级 4、盘点计划下载自动赋值checkResult为空 5、新增油网打包地址

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -33,8 +33,8 @@ export class HttpService {
       // this.setUrl("http","210.12.193.123","9081","plamassets");
       // return "http://210.12.193.123:9081/plamassets/mobile/";
       //福建
-      this.setUrl("http","210.12.193.92","9080","plamassets");
-      return "http://210.12.193.92:9080/plamassets/mobile/";
+      // this.setUrl("http","210.12.193.92","9080","plamassets");
+      // return "http://210.12.193.92:9080/plamassets/mobile/";
       //辽宁
       // this.setUrl("http","210.12.193.94","9081","plamassets");
       // return "http://210.12.193.94:9081/plamassets/mobile/";
@@ -59,6 +59,9 @@ export class HttpService {
       //黑龙江销售
       // this.setUrl("http","210.12.194.210","9080","plamassets");
       // return "http://210.12.194.210:9080/plamassets/mobile/"
+      //油网
+      this.setUrl("http","10.215.9.57","8081","plamassets");
+      return "http://10.215.9.57:8081/plamassets/mobile/"
     }
     if (this.storageService.getDevice()==2){
       this.setUrl("http","114.116.135.83","8080","plamassets");
