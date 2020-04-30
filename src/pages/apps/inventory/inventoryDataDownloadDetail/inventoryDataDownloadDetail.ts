f86eacbccc0c30b061b9818c095fbffd0af70691
731646594 <731646594@qq.com>
2020-04-30 15:53:48 +0800

    0.85 1、修复RFID扫码数据录入逻辑的bug 2、盘点-盘盈新增图片丢失提示 3、数据上传新增图片丢失时的上传逻辑和提示 4、修复数据下载时ios读取数据异常

--- src/pages/apps/inventory/inventoryDataDownloadDetail/inventoryDataDownloadDetail.ts
+++ src/pages/apps/inventory/inventoryDataDownloadDetail/inventoryDataDownloadDetail.ts
@@ -19,6 +19,15 @@ export class InventoryDataDownloadDetailPage {
   isOnfocus=false;
   displayArray = [];
   searchValue;
+  base64DecodeChars = [
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+    -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1];
   constructor(public navCtrl: NavController,public httpService:HttpService,public storageService:StorageService,
               public alertCtrl:AlertController,public loadingCtrl:LoadingController,public file:File,
               public fileTransfer:FileTransfer,public navParams:NavParams,public app:App) {
@@ -88,10 +97,12 @@ export class InventoryDataDownloadDetailPage {
             title:"下载成功！"
           });
           alertCtrl.present();
+          var reader = new FileReader();
           entry.file((file)=>{
-            var reader = new FileReader();
             reader.onloadend=(e)=>{
-              let data=JSON.parse(e.target['result']).data;
+              let str = e.target['result'].replace("data:text/plain;base64,","");
+              let letterStr = this.utf8To16(this.base64Toutf8(str));
+              let data=JSON.parse(letterStr).data;
               let departments = JSON.parse(JSON.stringify(this.plan.departments));
               this.planDate.departments = [];
               for (let i in this.departments){
@@ -112,7 +123,7 @@ export class InventoryDataDownloadDetailPage {
               this.storageService.deleteUserTable("newPlanDetail",this.userCode);
               PageUtil.pages["home"].inventoryNum = data.length;
             };
-            reader.readAsText(file);
+            reader.readAsDataURL(file);
           })
 
         },(error)=>{
@@ -131,6 +142,88 @@ export class InventoryDataDownloadDetailPage {
       alertCtrl.present();
       loading.dismiss();
     })
+  }
+  base64Toutf8(str){
+    var c1, c2, c3, c4;
+    var i, len, out;
+
+    len = str.length;
+    i = 0;
+    out = "";
+    while(i < len) {
+      /* c1 */
+      do {
+        c1 = this.base64DecodeChars[str.charCodeAt(i++) & 0xff];
+      } while(i < len && c1 == -1);
+      if(c1 == -1)
+        break;
+
+      /* c2 */
+      do {
+        c2 = this.base64DecodeChars[str.charCodeAt(i++) & 0xff];
+      } while(i < len && c2 == -1);
+      if(c2 == -1)
+        break;
+
+      out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+
+      /* c3 */
+      do {
+        c3 = str.charCodeAt(i++) & 0xff;
+        if(c3 == 61)
+          return out;
+        c3 = this.base64DecodeChars[c3];
+      } while(i < len && c3 == -1);
+      if(c3 == -1)
+        break;
+
+      out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+
+      /* c4 */
+      do {
+        c4 = str.charCodeAt(i++) & 0xff;
+        if(c4 == 61)
+          return out;
+        c4 = this.base64DecodeChars[c4];
+      } while(i < len && c4 == -1);
+      if(c4 == -1)
+        break;
+      out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+    }
+    return out;
+  }
+  utf8To16(str){
+    var out, i, len, c;
+    var char2, char3;
+
+    out = "";
+    len = str.length;
+    i = 0;
+    while(i < len) {
+      c = str.charCodeAt(i++);
+      switch(c >> 4)
+      {
+        case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+        // 0xxxxxxx
+        out += str.charAt(i-1);
+        break;
+        case 12: case 13:
+        // 110x xxxx   10xx xxxx
+        char2 = str.charCodeAt(i++);
+        out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+        break;
+        case 14:
+          // 1110 xxxx  10xx xxxx  10xx xxxx
+          char2 = str.charCodeAt(i++);
+          char3 = str.charCodeAt(i++);
+          out += String.fromCharCode(((c & 0x0F) << 12) |
+            ((char2 & 0x3F) << 6) |
+            ((char3 & 0x3F) << 0));
+          break;
+      }
+    }
+
+    return out;
   }
   downloadPlan(){
     let item=[];
