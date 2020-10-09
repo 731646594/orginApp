e08322029fe26e7f7b82c44c6e359f4d66e7c024
731646594 <731646594@qq.com>
2020-10-09 10:01:07 +0800

    1.0.5 1、修改版本号 2、使用新密码校验规则

--- src/pages/mine/login/login.ts
+++ src/pages/mine/login/login.ts
@@ -5,6 +5,7 @@ import {StorageService} from "../../../services/storageService";
 
 import {ServerSettingPage} from "../serverSetting/serverSetting";
 import {TabsPage} from "../../tabs/tabs";
+import {ModifyPasswordPage} from "../modifyPassword/modifyPassword";
 
 @Component({
   selector: 'page-login',
@@ -53,6 +54,40 @@ export class LoginPage {
         alert.present();
         return;
       }else {
+        //验证特殊字符
+        let patternSpecial=/(?=(?:.*?[·~!@#￥%^&*()_\|,<>.?/=+]))/;
+        //验证大写字母
+        let patternCapital = /(?=(?:.*?[A-Z]))/;
+        //验证密码长度
+        let patternLength=/(?=^.{12,}$)/;
+        //验证数字
+        let patternNumber=/(?=(?:.*?\d))/;
+        //验证小写字母
+        let patternLower= /(?=(?:.*?[a-z]))/;
+        //验证连续数字
+        //验证顺序连续数字不能超过五位(结果注意取反)
+        let patternContinuous= /(0(?=1)|1(?=2)|2(?=3)|3(?=4)|4(?=5)|5(?=6)|6(?=7)|7(?=8)|8(?=9)){4}\d/;
+        //验证倒序连续数字不能超过五位(结果注意取反)
+        let patternBackNumber= /(9(?=8)|8(?=7)|7(?=6)|6(?=5)|5(?=4)|4(?=3)|3(?=2)|2(?=1)|1(?=0)){4}\d/;
+        //验证五位重复数字(结果注意取反)
+        let patternRepeat=/([\d])\1{4}/;
+        if (
+          !(patternSpecial.test(this.password))||
+          !(patternCapital.test(this.password))||
+          !(patternLength.test(this.password))||
+          !(patternNumber.test(this.password))||
+          !(patternLower.test(this.password))||
+          (patternContinuous.test(this.password))||
+          (patternBackNumber.test(this.password))||
+          (patternRepeat.test(this.password))
+        ){
+          let alert1=this.alertCtrl.create({
+            title:"密码不符合检验规则请修改密码！"
+          });
+          alert1.present();
+          this.app.getRootNav().push(ModifyPasswordPage,{oldPsw:this.password});
+          return;
+        }
         let loginInfo = {};
         loginInfo = data.data;
         let item = loginInfo[2].func.replace(/'/g, '"');
--- src/pages/mine/modifyPassword/modifyPassword.ts
+++ src/pages/mine/modifyPassword/modifyPassword.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {AlertController, App, NavController} from 'ionic-angular';
+import {AlertController, App, NavController, NavParams} from 'ionic-angular';
 import {StorageService} from "../../../services/storageService";
 import {HttpService} from "../../../services/httpService";
 import {LoginPage} from "../login/login";
@@ -13,9 +13,23 @@ export class ModifyPasswordPage {
   oldPsw;
   newPsw;
   confirmPsw;
+  psdRules = [];
   constructor(public navCtrl: NavController,public httpService:HttpService,public storageService:StorageService,
-              public alertCtrl:AlertController,public app:App) {
+              public alertCtrl:AlertController,public app:App,public navParams:NavParams) {
     this.loadData();
+    if (this.navParams.get('oldPsw')){
+      this.oldPsw = this.navParams.get('oldPsw');
+    }
+    this.psdRules = [
+      '新密码必须含有特殊字符。',
+      '新密码最短为12位。',
+      '新密码必须含有大写字母。',
+      '新密码必须含有小写字母。',
+      '新密码必须含有数字。',
+      '新密码不能有5位以上正序连续数字。',
+      '新密码不能有5位以上倒序连续数字。',
+      '新密码不能有5位以上重复数字'
+    ]
   }
   loadData(){
     this.userCode = this.storageService.read("loginUserCode")
@@ -35,16 +49,75 @@ export class ModifyPasswordPage {
       alert.present();
       return;
     }
+    //验证特殊字符
+    let patternSpecial=/(?=(?:.*?[·~!@#￥%^&*()_\|,<>.?/=+]))/;
+    //验证大写字母
+    let patternCapital = /(?=(?:.*?[A-Z]))/;
+    //验证密码长度
+    let patternLength=/(?=^.{12,}$)/;
+    //验证数字
+    let patternNumber=/(?=(?:.*?\d))/;
+    //验证小写字母
+    let patternLower= /(?=(?:.*?[a-z]))/;
+    //验证连续数字
+    //验证顺序连续数字不能超过五位(结果注意取反)
+    let patternContinuous= /(0(?=1)|1(?=2)|2(?=3)|3(?=4)|4(?=5)|5(?=6)|6(?=7)|7(?=8)|8(?=9)){4}\d/;
+    //验证倒序连续数字不能超过五位(结果注意取反)
+    let patternBackNumber= /(9(?=8)|8(?=7)|7(?=6)|6(?=5)|5(?=4)|4(?=3)|3(?=2)|2(?=1)|1(?=0)){4}\d/;
+    //验证五位重复数字(结果注意取反)
+    let patternRepeat=/([\d])\1{4}/;
+    if (!(patternSpecial.test(this.newPsw))){
+      let alert1=this.alertCtrl.create({
+        title:"新密码必须含有特殊字符！"
+      });
+      alert1.present();
+      return;
+    }else if (!(patternCapital.test(this.newPsw))){
+      let alert1=this.alertCtrl.create({
+        title:"新密码必须含有大写字母！"
+      });
+      alert1.present();
+      return;
+    }else if (!(patternLength.test(this.newPsw))){
+      let alert1=this.alertCtrl.create({
+        title:"新密码最短为12位！"
+      });
+      alert1.present();
+      return;
+    }else if (!(patternNumber.test(this.newPsw))){
+      let alert1=this.alertCtrl.create({
+        title:"新密码必须含有数字！"
+      });
+      alert1.present();
+      return;
+    }else if (!(patternLower.test(this.newPsw))){
+      let alert1=this.alertCtrl.create({
+        title:"新密码必须含有小写字母！"
+      });
+      alert1.present();
+      return;
+    }else if ((patternContinuous.test(this.newPsw))){
+      let alert1=this.alertCtrl.create({
+        title:"新密码不能有5位以上正序连续数字！"
+      });
+      alert1.present();
+      return;
+    }else if ((patternBackNumber.test(this.newPsw))){
+      let alert1=this.alertCtrl.create({
+        title:"新密码不能有5位以上倒序连续数字！"
+      });
+      alert1.present();
+      return;
+    }else if ((patternRepeat.test(this.newPsw))){
+      let alert1=this.alertCtrl.create({
+        title:"新密码不能有5位以上重复数字！"
+      });
+      alert1.present();
+      return;
+    }
     if (!this.confirmPsw){
       let alert=this.alertCtrl.create({
         title:"确认密码不能为空！"
-      });
-      alert.present();
-      return;
-    }
-    if(!(/^(\w+|[!@#$%^&*]+){6,12}$/.test(this.newPsw))){
-      let alert=this.alertCtrl.create({
-        title:"密码只能输入6到12位的数字、字母或特殊字符！"
       });
       alert.present();
       return;
