fba44efbc36b4fff1e144046f14d47c32a2f2e21
731646594 <731646594@qq.com>
2020-10-27 15:19:02 +0800

    1.0.5 1、新增青海油田无飞行模式机制

--- src/pages/mine/login/login.ts
+++ src/pages/mine/login/login.ts
@@ -107,9 +107,9 @@ export class LoginPage {
           this.depart = this.departList[0];
           if (this.httpService.getUrl() == 'http://210.12.194.113:7000/plamassets/mobile/'||
             this.httpService.getUrl() == 'http://218.205.135.168:7000/plamassets/mobile/'){
-            this.storageService.write('noFlightMode',true)
+            this.storageService.write('noFlightMode',true);
           }else {
-            this.storageService.write('noFlightMode',false)
+            this.storageService.write('noFlightMode',false);
           }
         }catch {
           let alert=this.alertCtrl.create({
