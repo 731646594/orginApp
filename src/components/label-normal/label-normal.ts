f04b0e1f0470ebd2fa6544520a3c965106fbb18c
731646594 <731646594@qq.com>
2021-01-12 08:57:48 +0800

    1.0.7 1、新增维修和保养功能内，字段换行

--- src/components/label-normal/label-normal.ts
+++ src/components/label-normal/label-normal.ts
@@ -17,5 +17,16 @@ export class LabelNormalComponent {
   constructor() {
 
   }
-
+  ngOnInit(){
+    let last = document.getElementsByClassName('content').length -1;
+    let pageName = document.getElementsByClassName('content')[last].parentElement.tagName;
+    if (pageName.indexOf('repair') > 0 || pageName.indexOf('REPAIR') > 0 || pageName.indexOf('maintenance') > 0 || pageName.indexOf('MAINTENANCE') > 0){
+      let labelArray = document.getElementsByTagName(pageName)[0].getElementsByClassName('labelNormal');
+      for (let i = 0; i < labelArray.length;i++){
+        if (labelArray.item(i).className.indexOf('showAllLabel') == -1){
+          labelArray.item(i).className += ' showAllLabel'
+        }
+      }
+    }
+  }
 }
